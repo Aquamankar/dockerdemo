3f7c40702d120c60c6ab867476e5eb4b6b2cee2f
AkankshaMankar <89790286+Aquamankar@users.noreply.github.com>
2025-04-11 22:04:04 +0530

    Update democontroller.java

--- src/main/java/com/dockerpush/demo/controller/democontroller.java
+++ src/main/java/com/dockerpush/demo/controller/democontroller.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class democontroller {
 
+      @GetMapping("/hotels")
     public  String getMessage(){
         return "hello world";
     }
